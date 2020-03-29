23b0bcb6862652de866e88930237580bf11bafcb
Luiz Augusto <luiz.m.santiago@hotmail.com>
2020-03-29 10:05:09 -0300

    Calculadora iniciada

---
+++ script.js
@@ -0,0 +1,39 @@
+var a = Number;
+var b = Number;
+var temp = String;
+
+function botoes(valor)
+{
+
+}
+
+function mais(a, b)
+{
+    resultado = a + b;
+    igual()
+}
+
+function menos(a, b)
+{
+
+}
+
+function divisao(a, b)
+{
+
+}
+
+function multiplicação(a, b)
+{
+
+}
+
+function igual(resultado)
+{
+
+}
+
+function limpar()
+{
+
+}
